2746e598436aced1f9d450174d8f194274890d75
fidel palacios <herney400@gmail.com>
2022-01-18 20:57:19 -0500

    Funciones auxiliares de componentes

--- src/index.js
+++ src/index.js
@@ -2,83 +2,42 @@ import { paste } from '@testing-library/user-event/dist/paste'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  console.log(parts[0].exercises);
-  return (
-    <div>  
-      <Header nombre={course}/>
-      <Content parts={parts}/>
-      <Total  total={parts[0].exercises+parts[1].exercises+parts[2].exercises}/>
- 
-    </div>
-  )
-}
+/**
+ * 
+ * @param {*} props 
+ * @returns 
+ */
+const Hello=(props)=>{
+  
 
-/**Componenter Header  */
-
-const Header=(props)=>{
-  console.log(props)
+  const bornYear =()=>{
+    const yearNow=new Date().getFullYear()
+    return yearNow - props.age
+  }
   return (
      <div>
-       <p>El nombre de curso es: {props.nombre} </p>
+       <p>Hello {props.name}, you are {props.age} years old</p>
+       <p>So you were probably born in {bornYear()}</p>
      </div>
-  )  
-  
-
-}
-/**Componente Conntent */
-const Content=(props)=>{
-  
-  console.log(props)
- return (
-    <div>
-  
-       <Part  nombre={props.parts[0].name} exercise={props.parts[0].exercises}/> 
-       <Part  nombre={props.parts[1].name} exercise={props.parts[1].exercises}/> 
-       <Part  nombre={props.parts[2].name} exercise={props.parts[2].exercises}/> 
-
-        
-     </div>
-  )
-
-}
-/**componente Total */
-
-const Total =(props)=>{
-
-  return(
-    <div>
-      <p>Total ejercicios: {props.total} </p>
-    </div>
-  )
+  )   
 
 }
 
-/**Componente Part */
-
-const Part =(props)=>{
+const App = () => {
+   const name ='Peter'
+   const age =10
 
   return (
-    <div>
-      <p>Nombre del ejercicio: {props.nombre}, Número de ejercicios :{props.exercise} </p>
+    <div>  
+      <h1>Greetings</h1>
+      < Hello name={"May"} age={26+10} />
+      <Hello name={name} age={age}/>
     </div>
   )
 }
+
+
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'))
