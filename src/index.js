6d05c87373af055f7a702aed88f84047ec12a13e
fidel palacios <herney400@gmail.com>
2022-01-19 20:28:40 -0500

    Manejo de evento y estado

--- src/index.js
+++ src/index.js
@@ -1,18 +1,42 @@
 import { paste } from '@testing-library/user-event/dist/paste'
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-
-const App = (props) => {
+/**A continuación, agreguemos estado al componente App de nuestra aplicación 
+ * con la ayuda del hook de estado de React. Cambiaremos la aplicación a lo siguiente:*
+*/
+const App = () => {
  
-  const {counter}=props
-  
+  const [counter , setCounter]=useState(0)
+
+  const handleClick=()=>{
+    console.log("Clicked")
+  }
+
+/*  setTimeout(
+    ()=>setCounter(counter + 1), 
+    1000
+  )*/
+
+ 
   return (
-    <div>  
-      {counter}   
+    <div>
+    <div> {counter}</div>
+    <button onClick={()=>setCounter(counter+1)}>
+      plus
+
+    </button>
+    <button onClick={()=>setCounter(0)} >
+      Zero
+    </button>
     </div>
+    
   )
 } 
-
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
+/*
 let counter =1
 
 const refresh =() =>{
@@ -23,5 +47,5 @@ const refresh =() =>{
 setInterval(() => {
   refresh()
   counter += 1
-}, 1000)
+}, 1000) */
 
